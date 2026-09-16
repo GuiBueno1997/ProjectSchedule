73ff639ff840a4c0d96fa1dff291721a1dd768b6
GuiBueno1997 <guilhermecbueno97@hotmail.com>
2026-09-16 20:28:30 -0300

    Alter 2026-09-16

--- contact/forms.py
+++ contact/forms.py
@@ -3,37 +3,19 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class': 'classe-a',
-                'placeholder': 'Escreva aqui',
-            }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para o seu usuário'
-    )
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a',
-        #     'placeholder': 'Aqui init',
-        # })
+    picture=forms.ImageField(
+        widget=forms.FileInput(attrs={
+            'accept': 'image/*'
+        }
+    ))
 
     class Meta:
         model = models.Contact
         fields = ['first_name', 'last_name', 'phone',
-                  'email', 'description', 'category']
+                  'email', 'description', 'category',
+                  'picture']
 
-        # widgets={
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class': 'classe-a',
-        #             'placeholder': 'Escreva aqui',
-        #         }
-        #     )
-        # }
+   
 
     def clean(self):
         cleaned_data = self.cleaned_data
