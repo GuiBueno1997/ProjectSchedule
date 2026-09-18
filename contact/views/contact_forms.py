85fdae4a849a0a938f60830a352aab24d32fa445
GuiBueno1997 <guilhermecbueno97@hotmail.com>
2026-09-17 21:50:47 -0300

    2026-09-17

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from contact.forms import ContactForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
+
+from contact.forms import ContactForm
 from contact.models import Contact
 
+
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -26,16 +30,21 @@ def create(request):
 
     context = {
         'form': ContactForm(),
-        'form_action': form_action
+        'form_action': form_action,
     }
+
     return render(
         request,
         'contact/create.html',
         context
     )
 
+
+@login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
@@ -58,30 +67,32 @@ def update(request, contact_id):
 
     context = {
         'form': ContactForm(instance=contact),
-        'form_action': form_action
+        'form_action': form_action,
     }
+
     return render(
         request,
         'contact/create.html',
         context
     )
 
+
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
     confirmation = request.POST.get('confirmation', 'no')
 
     if confirmation == 'yes':
         contact.delete()
         return redirect('contact:index')
 
-    # contact.delete()
-    # return redirect('contact:index')
-
     return render(
         request,
         'contact/contact.html',
         {
             'contact': contact,
-            'confirmation': confirmation
+            'confirmation': confirmation,
         }
     )
